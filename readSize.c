0024280dc14a87632c5779187880f8e8de7d867f
Marko <marko.eremija@amres.ac.rs>
2017-07-15 19:39:08 +0200

    Read the size of different data types
    Read the size of different types of integer. Two additional header files included.

---
+++ readSize.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+int main(void) {
+
+unsigned long x = 0xabababab;
+
+uint32_t y;
+uint64_t z;
+
+printf("Char size is: %zd\n", sizeof(char));
+printf("Variable x size is: %zd\n", sizeof(x));
+printf("Size of unsigned long long is: %zd\n", sizeof(unsigned long long));
+printf("Size of unsigned int is: %zd\n", sizeof(y));
+printf("Size of short int is: %zd\n", sizeof(short int));
+printf("Size of unsigned long long is: %zd\n", sizeof(z));
+
+return EXIT_SUCCESS;
+
+}
