1f6e2218d2a58d9d20374ac6507c674a5c4e4151
Marko <marko.eremija@amres.ac.rs>
2017-07-15 19:45:24 +0200

    Change the text output
    Change the text output.

--- readSize.c
+++ readSize.c
@@ -11,12 +11,12 @@ unsigned long x = 0xabababab;
 uint32_t y;
 uint64_t z;
 
-printf("Char size is: %zd\n", sizeof(char));
-printf("Variable x size is: %zd\n", sizeof(x));
-printf("Size of unsigned long long is: %zd\n", sizeof(unsigned long long));
-printf("Size of unsigned int is: %zd\n", sizeof(y));
-printf("Size of short int is: %zd\n", sizeof(short int));
-printf("Size of unsigned long long is: %zd\n", sizeof(z));
+printf("Char size is %zd bytes.\n", sizeof(char));
+printf("Variable x size is %zd bytes.\n", sizeof(x));
+printf("Size of unsigned long long is %zd bytes.\n", sizeof(unsigned long long));
+printf("Size of unsigned int is %zd bytes.\n", sizeof(y));
+printf("Size of short int is %zd bytes.\n", sizeof(short int));
+printf("Size of unsigned long long is %zd bytes.\n", sizeof(z));
 
 return EXIT_SUCCESS;
 
